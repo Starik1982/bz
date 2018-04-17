99609447aa7228db8c0e999f194076769539c9e0
Starik1982 <s.situk@gmail.com>
2018-04-17 19:36:06 +0300

    statik on server2

--- bz/settings.py
+++ bz/settings.py
@@ -144,7 +144,7 @@ CKEDITOR_JQUERY_URL = os.path.join(STATIC_URL, 'js/jquery.min.js')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
-#try:
-#   from .settings_prod import *
-#except:
-#    pass
+try:
+   from .settings_prod import *
+except:
+    pass
