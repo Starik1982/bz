002fa66d39afeeb6ecb18bba66c7f6172e7f868f
Sergey Situk2 <Situk.Se@lkoprom.local>
2018-05-31 13:54:16 +0300

    31.05 14-00

---
+++ loginsys/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('birth_date')
--- loginsys/urls.py
+++ loginsys/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 	
     url(r'^login/$', views.login, name ='login'),
     url(r'^logout/$', views.logout, name ='logout'),
+    url(r'^register/$', views.register, name ='register'),
+
 
 ]
